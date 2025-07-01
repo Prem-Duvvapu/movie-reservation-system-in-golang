96950694facc4c56ca82104e4a75dd467c6a2162
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-07-01 19:49:09 +0000

    added controllers

---
+++ controllers/auth_controller.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"models"
+	"services"
+)
+
+type AuthController struct {
+	AuthService *services.AuthService
+}
+
+func NewAuthController(authService *services.AuthService) *AuthController {
+	return &AuthController {
+		AuthService: authService,
+	}
+}
+
+func (ac *AuthController) SignUp(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&User); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H("error": err.Error()))
+		return
+	}
+
+	newUser, err != ac.AuthService.SignUp(user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H("error": err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusCreated, newUser)
+}
+
+func (ac *AuthController) Login(c *gin.Context) {
+	var credentials models.Credentials
+	if err := c.ShouldBindJSON(&credentials); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, token, err := ac.AuthService.Login(credentials)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": error.Error()})
+		retun
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"user": user,
+		"token": token,
+	})
+}
+
+func (ac *AuthController) PromoteToAdmin(c *gin.Context) {
+	var userID = c.Param("userId")
+	if err := ac.AuthService.PromoteToAdmin(userID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H("error": err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User promoted to admin"})
+}
